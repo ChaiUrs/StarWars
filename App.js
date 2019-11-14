58f195b2b6c5b3eac2b12fe5975f0059506f5a1b
ChaiUrs <45889133+ChaiUrs@users.noreply.github.com>
2019-11-14 11:05:27 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import CardContainer from "./CardContainer";
+import NavBar from "./NavBar";
+import Header from "./Header";
+import Loader from "./Loader";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://swapi.co/api/people/";
+
+    fetch(url)
+      .then(response => response.json())
+      .then(people => this.setState({ data: people.results }));
+  }
+
+  render() {
+    const { data } = this.state;
+    return (
+      <div className="App">
+        {/*<NavBar /> */}
+        <Header />
+        {data.length === 0 ? (
+          <Loader /> //<h3>Loading Cards...</h3>
+        ) : (
+          <CardContainer data={data} />
+        ) //<h3>Cards Count: {data.length}</h3>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
